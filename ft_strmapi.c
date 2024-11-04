4f2ea5d1dfe6be476928ceb31b19f89a8adcd965
Hajar Charkaoui <hacharka@c2r5p6.1337.ma>
2024-11-04 16:23:39 +0100

    pushing ft-strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,26 +6,29 @@
 /*   By: hacharka <hacharka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:26:22 by hacharka          #+#    #+#             */
-/*   Updated: 2024/11/04 15:12:09 by hacharka         ###   ########.fr       */
+/*   Updated: 2024/11/04 15:50:35 by hacharka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    char *result;
-    unsigned int i;
+	char *result;
+	unsigned int i;
 
-    result = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
-    if(result == NULL)
-        return (NULL);
-    i = 0;
-    while (s[i] != '\0')
-    {
-        result[i] = f(i, s[i]);
-        i++;
-    }
-    result[i] = '\0';
-    return (result);
+	result = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if(result == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
 }
+
--- ft_tolower.c
+++ ft_tolower.c
@@ -6,13 +6,13 @@
 /*   By: hacharka <hacharka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 10:19:52 by hacharka          #+#    #+#             */
-/*   Updated: 2024/11/04 15:34:20 by hacharka         ###   ########.fr       */
+/*   Updated: 2024/11/04 15:53:07 by hacharka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int tolower(int c)
+int ft_tolower(int c)
 {
     if(c>= 'A' && c <= 'Z')
     {
